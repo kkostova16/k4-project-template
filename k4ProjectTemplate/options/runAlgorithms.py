156e3d6295ac456be40d8b137547ec747a8050d9
Katerina Kostova <kkostova@pcepsft11.dyndns.cern.ch>
2024-07-08 15:35:33 +0200

    An output file added

--- k4ProjectTemplate/options/runAlgorithms.py
+++ k4ProjectTemplate/options/runAlgorithms.py
@@ -6,6 +6,7 @@ from Configurables import (
 )
 from k4FWCore import ApplicationMgr
 from Configurables import EventDataSvc
+from k4FWCore import IOSvc
 
 producer = myProducer("Producer", OutputCollection=["MCParticles"])
 
@@ -14,6 +15,10 @@ transformer = myTransformer("Transformer", InputCollection=["MCParticles"],
 
 consumer = myConsumer("Consumer", MCInputCollection=["MCParticles"],
                                 RecoInputCollection = ["RecoParticles"], Offset=10)
+
+iosvc = IOSvc("IOSvc")
+iosvc.output = "output_k4test_runAlgoritms.root"
+iosvc.outputCommands = ["keep *"]
 
 ApplicationMgr(
     TopAlg=[producer, transformer, consumer],
